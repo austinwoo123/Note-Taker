4a634590e03a163bc9ad3976867738df45cbd9ab
Austin W <austinwoo123@gmail.com>
2021-01-25 19:27:02 -0800

    finish html routes and restructure folder

---
+++ server.js
@@ -0,0 +1,25 @@
+//Dependencies go here
+const express = require("express");
+const path = require("path");
+
+// Set up Express App
+const app = express()
+
+const PORT = process.env.port || 8080;
+
+//Sets up the Express app to handle data parsing or the middleland
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// require("./apiRoutes")(app);
+require("./htmlRoutes")(app);
+
+
+
+
+//Starts the server to begin listening
+app.listen(PORT, function () {
+    console.log("Application is listening on PORT " + PORT);
+});
