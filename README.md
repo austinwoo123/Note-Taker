cbb7a6000abd030bb635b9b8c09b22881d2ad428
Austin W <austinwoo123@gmail.com>
2021-01-26 23:45:11 -0800

    add screen shot and work on the api routes

--- README.md
+++ README.md
@@ -1,32 +1,48 @@
 # Note-Taker
 
 ## Description
-
+I had to create an application that can be used to write, save, and delete notes. This application will use an express backend and save and retrieve note date from a JSON file. 
 ## Table of Contents
 1. [Description](#description)
 2. [Screenshot](#Screenshot)
-3. [Walkthrough Video](#Walkthrough-Video)
-4. [Installation](#Installation)
-5. [Code Snippet](#code-snippet)
-6. [Built with](#built-with)
-7. [Licenses](#licenses)
-8. [Author](#author)
-9. [Questions](#Questions)
-10. [Acknowledgements](#acknowledgements)
+3. [Installation](#Installation)
+4. [Code Snippet](#code-snippet)
+5. [Built with](#built-with)
+6. [Licenses](#licenses)
+7. [Author](#author)
+8. [Questions](#Questions)
+9. [Acknowledgements](#acknowledgements)
 
 ## Screenshot
 
-## Walkthrough Video
 
 ## Installation
-
+1. Clone from GitHub
+2. Open project directory, then npm install to install all dependencies
 ## Code Snippet
+```
+function getNotes() {
+        fs.readFile("./db/db.json", 'utf8', (err, data) => {
+            if (err) throw err;
+            return JSON.parse(data);
+        });
+    }
+    // create get route for /api/notes
+    app.get("/api/notes", function (req, res) {
+        // console.log("hello world!")
+
+        let notes = getNotes();
+        res.json(notes);
+    });
+```
+This code shows how I created my get routes in my apiRoutes.js
+
 
 ## Built with
 - Node
 - Gitbash
 - Express
-- 
+
 
 
 ## Licenses
--- apiRoutes.js
+++ apiRoutes.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require('path');
 const notes = require('./db/db.json');
+
 // console.log("notes: ", notes);
 
 // fs.writeFile
@@ -10,7 +11,7 @@ module.exports = function (app) {
 
     //readfile to setup notes variable
     function getNotes() {
-        fs.readFile("db/db.json", 'utf8', (err, data) => {
+        fs.readFile("./db/db.json", 'utf8', (err, data) => {
             if (err) throw err;
             return JSON.parse(data);
         });
@@ -29,7 +30,7 @@ module.exports = function (app) {
     });
     // Create post route for /api/notes 
     app.post("/api/notes", function (req, res) {
-
+        notesId = notes.length + 1
         // fs.readFile("/db/db.json", JSON.parse (res), function (err,data){
         //     if (err) throw err;
         //     var dbNotes= JSON.parse(data);
@@ -44,26 +45,50 @@ module.exports = function (app) {
         }
         // let noteId =
         notes.push(createNote);
+        // for (let i = 0; i < db.length; i++) {
+        //     const newNote = {
+        //         title: db[i].title,
+        //         text: db[i].text,
+        //         id: i
+        //     };
+        // }
+
+        newNoteList.push(newNote);
         // writeDb();
-        fs.writeFile(path.join(__dirname, "../db/db.json", JSON.stringify(notes, '\t'), err => {
-            if (err) throw err;
-
-            res.json(createNote)
-        }))
-
-        return console.log("New note added: " + createNote.title);
-    });
-    //function that updates the db.json file when there is a new note. 
-    function writeDb() {
-        // getNotes();
         fs.writeFile("./db/db.json", JSON.stringify(notes, '\t'), err => {
             if (err) throw err;
-
-            res.json(req.body)
         })
+        addnote(req.body)
+        res.json(createNote)
     }
+        // function addNote(note) {
+        //     const newNote = { title, text, id: uuidv1() };
+        // }
 
-    app.delete("/api/notes/:id", function (req, res) {
+        //     function addNote{
+
+        //         var note = {
+        //             title: 
+        //             text: 
+        //             id:
+        //         }
+
+        //     }
+        // })
+
+        // return console.log("New note added: " + createNote.title);
+
+        //function that updates the db.json file when there is a new note. 
+        // function writeDb() {
+        //     // getNotes();
+        //     fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+        //         if (err) throw err;
+
+        //         res.json(req.body)
+        //     })
+        // }
+
+        app.delete("/api/notes/:id", function (req, res) {
         notes.splice(req.params.id, 1);
         writeDb();
         console.log("You deleted the note with id " + req.params.id);
@@ -72,24 +97,29 @@ module.exports = function (app) {
 
 
 
+    // addNote(note) {
+    //     const { title, text } = note;
+    //     if (!title || !text) {
+    //       throw new _Error_(“Note ‘title’ and ‘text’ cannot be blank”);
+    //     }
+    //     // Add a unique id to the note using uuid package
+    //     const newNote = { title, text, id: uuidv1() };
+    //     // Get all notes, add the new note, write all the updated notes, return the newNote
+    //     return this.getNotes()
+    //       .then((notes) => [...notes, newNote])
+    //       .then((updatedNotes) => this.write(updatedNotes))
+    //       .then(() => newNote);
+    //   }
 
 
 
 
 
-
-
-
-
-
-
-
-
+    //  11:32
+    //  that is the complete function.  You will have to figure out where to put it.
 
 
 }
-
-
 
 
 
