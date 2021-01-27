6792b01f36efc2a17f0dac668d2ff9ffa4385ecb
Austin W <austinwoo123@gmail.com>
2021-01-26 20:09:37 -0800

    edit fs read file function in api routes file

--- apiRoutes.js
+++ apiRoutes.js
@@ -10,46 +10,46 @@ module.exports = function (app) {
     fs.readFile("db/db.json", 'utf8', (err, data) => {
         if (err) throw err;
         let notes = JSON.parse(data);
-
-        // create get route for /api/notes
-        app.get("/api/notes", function (req, res) {
-            console.log("hello world!")
-            res.json(notes);
-        });
-        // Create post route for /api/notes 
-        app.post("/api/notes", function (req, res) {
-            let createNote = req.body;
-            notes.push(createNote);
-            writeDb()
-            return console.log("New note added: " + createNote.title)
-        });
-        //function that upddates the db.json file when there is a new note. 
-        function writeDb() {
-            fs.writeFile("db/db.json", JSON.stringify(notes, '\n'), err => {
-                if (err) throw err;
-                return true;
-            })
-        }
-
-        app.get("/api/notes/:id", function (req, res) {
-            res.json(notes[req.params.id]);
-        });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     });
+    // create get route for /api/notes
+    app.get("/api/notes", function (req, res) {
+        console.log("hello world!")
+        res.json(notes);
+    });
+    // Create post route for /api/notes 
+    app.post("/api/notes", function (req, res) {
+        let createNote = req.body;
+        notes.push(createNote);
+        writeDb()
+        return console.log("New note added: " + createNote.title)
+    });
+    //function that upddates the db.json file when there is a new note. 
+    function writeDb() {
+        fs.writeFile("db/db.json", JSON.stringify(notes, '\n'), err => {
+            if (err) throw err;
+            return true;
+        })
+    }
+
+    app.get("/api/notes/:id", function (req, res) {
+        res.json(notes[req.params.id]);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
