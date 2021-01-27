7f7b0b9e3391d604dbdd6bbebd51fe688187c1e7
Austin W <austinwoo123@gmail.com>
2021-01-26 23:03:38 -0800

    edit post route

--- apiRoutes.js
+++ apiRoutes.js
@@ -1,12 +1,13 @@
 const fs = require("fs");
 const path = require('path');
 const notes = require('./db/db.json');
-console.log("notes: ", notes);
+// console.log("notes: ", notes);
 
 // fs.writeFile
 
 
 module.exports = function (app) {
+
     //readfile to setup notes variable
     function getNotes() {
         fs.readFile("db/db.json", 'utf8', (err, data) => {
@@ -17,29 +18,50 @@ module.exports = function (app) {
     // create get route for /api/notes
     app.get("/api/notes", function (req, res) {
         // console.log("hello world!")
+
         let notes = getNotes();
         res.json(notes);
     });
+
+    app.get("/api/notes/:id", function (req, res) {
+        // getNotes();
+        res.json(notes[req.params.id]);
+    });
     // Create post route for /api/notes 
     app.post("/api/notes", function (req, res) {
+
+        // fs.readFile("/db/db.json", JSON.parse (res), function (err,data){
+        //     if (err) throw err;
+        //     var dbNotes= JSON.parse(data);
+        //     var noteArray = [];
+
+        // })
         let createNote = req.body;
+        if (notes.length === 0) {
+            createNote.id = 1
+        } else {
+            createNote.id = (notes[notes.length - 1].id + 1);
+        }
+        // let noteId =
         notes.push(createNote);
-        writeDb();
+        // writeDb();
+        fs.writeFile(path.join(__dirname, "../db/db.json", JSON.stringify(notes, '\t'), err => {
+            if (err) throw err;
+
+            res.json(createNote)
+        }))
+
         return console.log("New note added: " + createNote.title);
     });
     //function that updates the db.json file when there is a new note. 
     function writeDb() {
-        getNotes();
-        fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
+        // getNotes();
+        fs.writeFile("./db/db.json", JSON.stringify(notes, '\t'), err => {
             if (err) throw err;
-            return true;
+
+            res.json(req.body)
         })
     }
-
-    app.get("/api/notes/:id", function (req, res) {
-        getNotes();
-        res.json(notes[req.params.id]);
-    });
 
     app.delete("/api/notes/:id", function (req, res) {
         notes.splice(req.params.id, 1);
