94f582271e096deb8eece963095c769bcfda51ac
Austin W <austinwoo123@gmail.com>
2021-01-27 19:39:01 -0800

    add start script for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 // Set up Express App
 const app = express()
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 
 //Sets up the Express app to handle data parsing or the middleland
 app.use(express.urlencoded({ extended: true }));
